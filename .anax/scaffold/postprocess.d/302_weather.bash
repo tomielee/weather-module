7580e2affd53b87e50b4fac2dda2c3449c3d2e07
jeneljenel <jwwlee@gmail.com>
2019-12-05 09:29:08 +0100

    added curl model to bash.

--- .anax/scaffold/postprocess.d/302_weather.bash
+++ .anax/scaffold/postprocess.d/302_weather.bash
@@ -8,5 +8,6 @@
 # Copy the configuration files
 rsync -av vendor/jeneljenel/weather-module/config ./
 rsync -av vendor/jeneljenel/weather-module/src/Weather ./src
+rsync -av vendor/jeneljenel/weather-module/src/Curl ./src
 rsync -av vendor/jeneljenel/weather-module/view/weather ./view
 
