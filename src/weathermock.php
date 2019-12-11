280ce779c07746885c1b92cb8f4684b5cb73e0b3
jeneljenel <jwwlee@gmail.com>
2019-12-11 13:05:15 +0100

    added tests and test/config files

---
+++ src/weathermock.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* return [
+*       "darksky_key" => "yoursecretkey",
+*       "darksky_url" => "url ",
+* ];
+*/
+
+return [
+    "key" => "12345mock",
+    "baseUrl" => "http://localhost:8080/dbwebb/ramverk1-a/a/htdocs/weather/mock",
+    "exclude" => "",
+    "excludeMulti" => "",
+    "test" => true,
+    "geoUrl" => "http://localhost:8080/dbwebb/ramverk1-a/a/htdocs/weather/geomock"
+];
